4f225bc3498ad934940160655c64308f1825f50e
Erick Estrada <eof105@gmail.com>
2022-10-24 13:37:35 -0500

    Widget Animated Container

--- lib/router/app_routes.dart
+++ lib/router/app_routes.dart
@@ -6,18 +6,20 @@ class AppRoutes {
 
   static final menuOptions = <MenuOption>[
     // TODO: borrar home
-    MenuOption(route: 'home', icon: Icons.home, name: 'Home Screen', screen: const HomeScreen()),
+    // MenuOption(route: 'home', icon: Icons.home, name: 'Home Screen', screen: const HomeScreen()),
     MenuOption(route: 'listview1', icon: Icons.list, name: 'ListView Tipo 1', screen: const Listview1Screen()),
     MenuOption(route: 'listview2', icon: Icons.list_sharp, name: 'ListView Tipo 2', screen: const Listview2Screen()),
     MenuOption(route: 'alert', icon: Icons.add_alert_outlined, name: 'Alert Screen', screen: const AlertScreen()),
     MenuOption(route: 'card', icon: Icons.credit_card, name: 'Card Screen', screen: const CardScreen()),
     MenuOption(route: 'avatar', icon: Icons.supervised_user_circle_outlined, name: 'Circle Avatar', screen: const AvatarScreen()),
+    MenuOption(route: 'animated', icon: Icons.play_circle_outline_rounded, name: 'Animated Screen', screen: const AnimatedScreen()),
   ];
 
   static const initialRoute = 'home';
 
   static Map<String, Widget Function(BuildContext)> getAppRoutes() {
     Map<String, Widget Function(BuildContext)> appRoutes = {};
+    appRoutes.addAll({'home' : (context) => const HomeScreen()});
     for (final option in menuOptions) {
       appRoutes.addAll({option.route: (context) => option.screen});
     }
